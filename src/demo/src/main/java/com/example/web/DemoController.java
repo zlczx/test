1eacfb05f848d150b44b26dfcfbb52c561e6e672
zhaoliuchang <0049001198@znv.com>
2019-09-05 14:59:11 +0800

    测试redis

--- src/demo/src/main/java/com/example/web/DemoController.java
+++ src/demo/src/main/java/com/example/web/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,16 @@ public class DemoController {
 	
 	@RestController
 	public class HelloWorldController {
+    	 
+    	@Autowired
+    	StringRedisTemplate stringRedisTemplate;
+    	
 	    @RequestMapping("/hello")
 	    public String index() {
-	        return "Hello World";
+	    	
+	    	 String studentList= String.valueOf(stringRedisTemplate.opsForValue().get("00001001000000000021"));
+	    	
+	        return "Hello World:" + studentList;
 	    }
 	}
 }
